d035dfe9480a3bc0d307ec7a0984291821e74c9d
Nillo01 <127193018+Nillo01@users.noreply.github.com>
2025-11-17 16:32:20 -0300

    Implement basic heap operations in main function

--- main.c
+++ main.c
@@ -1,1 +1,40 @@
 
+#include "heap.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+
+int main(){
+    bool rodando = true;
+    int tam;
+    printf("digite um tamanho inicial para sua arvore heap");
+    scanf("%d", &tam);
+    heap* h = heap_criar(tam);
+    while(rodando){
+        int estado;
+        printf("1 - inserir\n2- remover\n3 - imprimir heap\n4 - sair\n");
+        scanf("%d", &estado);
+        switch(estado){
+
+            case 1:{
+                int num;
+                printf("digite o numero que deseja inserir\n");
+                scanf("%d", &num);
+                insere(h, num);
+                break;
+            }
+            case 2:
+                heap_remove(h);
+                break;
+            case 3:
+                imprimir_heap(h);
+                break;
+            case 4:
+                rodando = false;
+        }
+
+    }
+    return 0;
+}
